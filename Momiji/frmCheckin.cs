f46a0ed68558994bf4b69eb8e5c026c497a59d11
Jeremy Newton <alexjnewt@hotmail.com>
2016-04-14 11:34:07 -0400

    Initial work on Checkin

--- Momiji/frmCheckin.cs
+++ Momiji/frmCheckin.cs
@@ -1,4 +1,6 @@
 using System;
+using Gtk;
+using MySql.Data.MySqlClient;
 
 namespace Momiji
 {
@@ -12,6 +14,50 @@ namespace Momiji
 		private int artistID;
 
 		/////////////////////////
+		//  Private Functions  //
+		/////////////////////////
+
+		private void RefreshInfo ()
+		{
+			//TODO// Clear tables, create tables, nodes
+			SQL SQLConnection = parent.currentSQLConnection;
+			MySqlCommand info = new MySqlCommand("SELECT * FROM `artists` WHERE `ArtistID`=@ID",
+													SQLConnection.GetConnection());
+			info.Prepare();
+			info.Parameters.AddWithValue("@ID", artistID);
+
+			SQLResult result = SQLConnection.Query(info);
+
+			if (result.getCell("ArtistCheckIn", 0) == "True") {
+				MessageBox.Show(this, MessageType.Warning,
+								"Warning, this artist is already checked in.");
+				btnCheckIn.Sensitive = false;
+			}
+
+			//TODO// Load info into form
+
+			MySqlCommand merchData = new MySqlCommand("SELECT * FROM `merchandise` WHERE `ArtistID` = @ID;",
+														SQLConnection.GetConnection());
+			merchData.Prepare();
+			merchData.Parameters.AddWithValue("@ID", artistID);
+
+			SQLResult merchResults = SQLConnection.Query(merchData);
+			if (merchResults.GetNumberOfRows() > 0) {
+				//TODO// Load merch into form
+			}
+
+			MySqlCommand GSmerchData = new MySqlCommand("SELECT * FROM `GSmerchandise` WHERE `ArtistID` = @ID;",
+														SQLConnection.GetConnection());
+			GSmerchData.Prepare();
+			GSmerchData.Parameters.AddWithValue("@ID", artistID);
+
+			SQLResult GSmerchResults = SQLConnection.Query(GSmerchData);
+			if (GSmerchResults.GetNumberOfRows() > 0) {
+				//TODO// Load gsmerch into form
+			}
+		}
+
+		/////////////////////////
 		//     Contructor      //
 		/////////////////////////
 
@@ -21,40 +67,50 @@ namespace Momiji
 			this.parent = parent;
 			this.artistID = artistID;
 			this.Build ();
+
+			RefreshInfo();
 		}
 
 		/////////////////////////
 		//     GTK Signals     //
 		/////////////////////////
 
-		//TODO//
-
 		protected void OnBtnCancelClicked (object sender, EventArgs e)
 		{
-#if DEBUG
-			throw new System.NotImplementedException ();
-#endif
+			this.Destroy ();
 		}
 
 		protected void OnBtnEditArtistClicked (object sender, EventArgs e)
 		{
 #if DEBUG
+			//TODO// Child forms aren't done
 			throw new System.NotImplementedException ();
 #endif
 		}
 
 		protected void OnBtnReloadClicked (object sender, EventArgs e)
 		{
-#if DEBUG
-			throw new System.NotImplementedException ();
-#endif
+			//TODO// This should be done automatically
+			RefreshInfo();
 		}
 
 		protected void OnBtnCheckInClicked (object sender, EventArgs e)
 		{
-#if DEBUG
-			throw new System.NotImplementedException ();
-#endif
+			SQL SQLConnection = parent.currentSQLConnection;
+			MySqlCommand update = new MySqlCommand("UPDATE `artists` SET `ArtistCheckIn`=1 WHERE  `ArtistID`=@ID",
+													SQLConnection.GetConnection());
+			update.Prepare();
+			update.Parameters.AddWithValue("@ID", artistID);
+
+			SQLResult result = SQLConnection.Query(update);
+			if (result.successful())
+				MessageBox.Show(this, MessageType.Error,
+								"Artist checked in!");
+			else
+				MessageBox.Show (this, MessageType.Error,
+								"Connection Error, please try again.");
+
+			this.Destroy ();
 		}
 	}
 }
