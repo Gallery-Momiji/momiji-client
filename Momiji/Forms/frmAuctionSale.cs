af66c1eafa93314e604c113f9ecb4a2690f21cc7
Mystro256 <alexjnewt@hotmail.com>
2019-05-20 00:23:12 -0400

    Move query in Auction sale
    So we can catch an issue earlier

--- Momiji/Forms/frmAuctionSale.cs
+++ Momiji/Forms/frmAuctionSale.cs
@@ -13,6 +13,8 @@ namespace Momiji
 
 		private int ArtistID;
 		private int MerchID;
+		private string MerchTitle;
+		private float MinBid;
 
 		/////////////////////////
 		//  Private Functions  //
@@ -74,9 +76,38 @@ namespace Momiji
 					OnBtnClearClicked(sender, e);
 					return;
 				}
-				txtBarcode.Sensitive = false;
-				txtPrice.Sensitive = true;
-				txtPrice.GrabFocus();
+
+				SQL SQLConnection = parent.currentSQLConnection;
+				MySqlCommand query = new MySqlCommand("SELECT `MerchTitle`,`MerchMinBid`,`MerchSold` FROM `merchandise` WHERE `ArtistID` = @AID AND `MerchID` = @MID;",
+										 SQLConnection.GetConnection());
+				query.Prepare();
+				query.Parameters.AddWithValue("@AID", ArtistID);
+				query.Parameters.AddWithValue("@MID", MerchID);
+				SQLResult results = SQLConnection.Query(query);
+
+				if (results.GetNumberOfRows() < 1)
+				{
+					MessageBox.Show(this, MessageType.Error,
+						"Could not find merchandise in the database");
+					txtBarcode.Text = "";
+				}
+				else if (results.getCell("MerchSold", 0) == "1")
+				{
+					MessageBox.Show(this, MessageType.Error,
+						"This item has already been sold. This will be reported.");
+
+					SQLConnection.LogAction("Attempted to auction sell an already sold item (" + txtBarcode.Text + ")",
+						parent.currentUser);
+					txtBarcode.Text = "";
+				}
+				else
+				{
+					MerchTitle = results.getCell("MerchTitle", 0);
+					MinBid = float.Parse(results.getCell("MerchMinBid", 0));
+					txtBarcode.Sensitive = false;
+					txtPrice.Sensitive = true;
+					txtPrice.GrabFocus();
+				}
 			}
 			else
 			{
@@ -92,14 +123,6 @@ namespace Momiji
 				return;
 			}
 
-			if (ExistsInList(txtBarcode.Text))
-			{
-				txtBarcode.Text = "";
-				txtPrice.Text = "";
-				txtBarcode.GrabFocus();
-				return;
-			}
-
 			//Catch an invalid price
 			float Price;
 			if (!float.TryParse(txtPrice.Text, out Price))
@@ -109,63 +132,34 @@ namespace Momiji
 
 				txtPrice.Text = "";
 				txtPrice.GrabFocus();
-				return;
 			}
-
-			SQL SQLConnection = parent.currentSQLConnection;
-			MySqlCommand query = new MySqlCommand("SELECT `MerchTitle`,`MerchMinBid`,`MerchSold` FROM `merchandise` WHERE `ArtistID` = @AID AND `MerchID` = @MID;",
-									 SQLConnection.GetConnection());
-			query.Prepare();
-			query.Parameters.AddWithValue("@AID", ArtistID);
-			query.Parameters.AddWithValue("@MID", MerchID);
-			SQLResult results = SQLConnection.Query(query);
-
-			if (results.GetNumberOfRows() == 1)
+			else if (MinBid > Price)
 			{
-				float minbid = float.Parse(results.getCell("MerchMinBid", 0));
-				if (minbid > Price)
-				{
-					MessageBox.Show(this, MessageType.Error,
-						"Price is too low.\nThis item has a minimum bid of $" +
-						String.Format("{0:0.00}", minbid));
-					txtPrice.Text = "";
-					txtPrice.GrabFocus();
-					return;
-				}
-				else if (results.getCell("MerchSold", 0) == "1")
-				{
-					MessageBox.Show(this, MessageType.Error,
-						"This item has already been sold. This will be reported.");
-
-					SQLConnection.LogAction("Attempted to auction sell an already sold item (" + txtBarcode.Text + ")",
-						parent.currentUser);
-				}
-				else
-				{
-					merchStore.AddNode(new MerchNode(ArtistID,
-						MerchID,
-						results.getCell("MerchTitle", 0),
-						"$" + String.Format("{0:0.00}", Price)));
-
-					total = total + Price;
-					txtTotal.Text = String.Format("{0:0.00}", total);
-
-					items = items + txtBarcode.Text + "#";
-					prices = prices + Price.ToString() + "#";
-
-					btnPay.Sensitive = true;
-					txtPaid.Sensitive = true;
-					drpPaymentType.Sensitive = true;
-					btnCancel.Sensitive = true;
-				}
+				MessageBox.Show(this, MessageType.Error,
+					"Price is too low.\nThis item has a minimum bid of $" +
+					String.Format("{0:0.00}", MinBid));
+				txtPrice.Text = "";
+				txtPrice.GrabFocus();
 			}
 			else
 			{
-				MessageBox.Show(this, MessageType.Error,
-					"Could not find merchandise in the database");
-			}
+				merchStore.AddNode(new MerchNode(ArtistID,
+					MerchID,
+					MerchTitle,
+					"$" + String.Format("{0:0.00}", Price)));
 
-			OnBtnClearClicked(sender, e);
+				total = total + Price;
+				txtTotal.Text = String.Format("{0:0.00}", total);
+
+				items = items + txtBarcode.Text + "#";
+				prices = prices + Price.ToString() + "#";
+
+				btnPay.Sensitive = true;
+				txtPaid.Sensitive = true;
+				drpPaymentType.Sensitive = true;
+				btnCancel.Sensitive = true;
+				OnBtnClearClicked(sender, e);
+			}
 		}
 
 		protected void OnTxtPriceActivated(object sender, EventArgs e)
